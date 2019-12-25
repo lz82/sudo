c90ec6e67194fcbeadaf9e1fd71d2cba42f4db65
liuzhuang <70458055@qq.com>
2019-12-25 09:19:16 +0800

    :sparkles: init

---
+++ precache-manifest.bad75227313bf74fd9403267c6ccbd7f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "266a731e615cf2ee73b79cd143ce04b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af6069c39862b166bb0b",
+    "url": "/static/css/2.a3bee108.chunk.css"
+  },
+  {
+    "revision": "070fd7220d5de75b3532",
+    "url": "/static/css/main.115b8caa.chunk.css"
+  },
+  {
+    "revision": "af6069c39862b166bb0b",
+    "url": "/static/js/2.18f33d02.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.18f33d02.chunk.js.LICENSE"
+  },
+  {
+    "revision": "070fd7220d5de75b3532",
+    "url": "/static/js/main.a84c2db1.chunk.js"
+  },
+  {
+    "revision": "3d02fc0cd002da0fdfed",
+    "url": "/static/js/runtime-main.8b56aca9.js"
+  },
+  {
+    "revision": "313f0c10aeb72cc0347d2ac2b7826c29",
+    "url": "/static/media/iconfont.313f0c10.ttf"
+  },
+  {
+    "revision": "433323dc0fd763050cc93ece6e15ba11",
+    "url": "/static/media/iconfont.433323dc.woff"
+  },
+  {
+    "revision": "dc543acaf0481beb571a9af7ca0c4de0",
+    "url": "/static/media/iconfont.dc543aca.eot"
+  },
+  {
+    "revision": "e6f90de96af4b52e422d80fb2e3db595",
+    "url": "/static/media/iconfont.e6f90de9.svg"
+  }
+]);
